be7f3804f9d13d2922b9a17fff0e442e9b0b01f2
chandn33 <misschandni_patel003@hotmail.co.uk>
2023-06-25 17:28:28 +0100

    added cities
    a code has been created to allow users to search for any city around the world which will give them the value

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
+function search(city) {
 let apiKey = "13c250c9139660bdb6fb8b5c81e33bd0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
