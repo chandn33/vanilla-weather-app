5365351803d51fa219761f3bc090c784d7d947ec
chandn33 <misschandni_patel003@hotmail.co.uk>
2023-06-25 16:32:49 +0100

    change of time
    changed time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = dayOfWeek[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  let temperatureElement=document.querySelector("#temperature");
-  let cityElement=document.querySelector("#city");
-  temperatureElement.innerHTML = Math.round (response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "13c250c9139660bdb6fb8b5c81e33bd0";
